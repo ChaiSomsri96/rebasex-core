40605f3da68f36b9fb2456a501fed55a0637ff0b
ChaiSomsri96 <chaisomsri96@gmail.com>
2024-12-10 04:12:00 +0700

    feat: internal function update

--- src/RebaseXERC20.sol
+++ src/RebaseXERC20.sol
@@ -28,4 +28,100 @@ contract RebaseXERC20 is IRebaseXERC20 {
      * @inheritdoc IRebaseXERC20
      */
     bytes32 public immutable DOMAIN_SEPARATOR;
+
+    /**
+     * @inheritdoc IRebaseXERC20
+     * @dev Value should equal `0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9` but it
+     *   is recommended to verify this by checking the public method on-chain.
+     */
+    bytes32 public constant PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+    /**
+     * @inheritdoc IRebaseXERC20
+     */
+    mapping(address => uint256) public nonces;
+
+    constructor() {
+        uint256 chainId;
+        assembly {
+            chainId := chainid()
+        }
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes("RebaseX")),
+                keccak256(bytes("1")),
+                chainId,
+                address(this)
+            )
+        );
+    }
+
+    /**
+     * @dev Mints `value` tokens to `to`.
+     *
+     * Emits a {IRebaseXERC20Events-Transfer} event.
+     * @param to The account that is receiving the tokens
+     * @param value The amount of tokens being created
+     */
+    function _mint(address to, uint256 value) internal {
+        totalSupply = totalSupply + value;
+        balanceOf[to] = balanceOf[to] + value;
+        emit Transfer(address(0), to, value);
+    }
+
+    /**
+     * @dev Burns `value` tokens from `from`.
+     *
+     * Emits a {IRebaseXERC20Events-Transfer} event.
+     * @param from The account that is sending the tokens
+     * @param value The amount of tokens being destroyed
+     */
+    function _burn(address from, uint256 value) internal {
+        balanceOf[from] = balanceOf[from] - value;
+        totalSupply = totalSupply - value;
+        emit Transfer(from, address(0), value);
+    }
+
+    /**
+     * @dev Sets `value` as the allowance of `spender` over the caller's tokens.
+     *
+     * Emits a {IRebaseXERC20Events-Approval} event.
+     * @param owner The account whose tokens are being approved
+     * @param spender The account that is granted permission to spend the tokens
+     * @param value The amount of tokens that can be spent
+     */
+    function _approve(address owner, address spender, uint256 value) private {
+        allowance[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+    /**
+     * @dev Moves `value` tokens from `from` to `to`.
+     *
+     * Emits a {IRebaseXERC20Events-Transfer} event.
+     * @param from The account that is sending the tokens
+     * @param to The account that is receiving the tokens
+     * @param value The amount of tokens being sent
+     */
+    function _transfer(address from, address to, uint256 value) internal {
+        balanceOf[from] = balanceOf[from] - value;
+        balanceOf[to] = balanceOf[to] + value;
+        emit Transfer(from, to, value);
+    }
+
+    /**
+     * @inheritdoc IRebaseXERC20
+     */
+    function name() external view virtual override returns (string memory _name) {
+        _name = "RebaseX";
+    }
+
+    /**
+     * @inheritdoc IRebaseXERC20
+     */
+    function symbol() external view virtual override returns (string memory _symbol) {
+        _symbol = "RX";
+    }
 }
